2273642079ee87c388a809663d5286d456bc87c3
Yash Sharma <yash304sharma@gmail.com>
2023-09-01 22:55:52 +0530

    Create RemoveNthNodeFromEndOfList.java

---
+++ RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,28 @@
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode temp=head;
+        int size=0;
+        while(temp!=null){
+            size++;
+            temp=temp.next;
+        }
+        if(size==1){
+            head=null;
+            return head;
+        }
+        temp=head;
+        if(size==n){
+            head=head.next;
+            return head;
+        }
+        size=size-n-1;
+        int a=0;
+        temp=head;
+        while(a!=size){
+            temp=temp.next;
+            a++;
+        }
+        temp.next=temp.next.next;
+        return head;
+    }
+}
